0e504358227e6be0e0b8cd9308b672fa230517a0
Peter McHale <peter.thomas.mchale@gmail.com>
2017-10-06 22:07:17 -0700

    lec 4 edits

---
+++ coding_homework/HW01/libHW01.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def drawfunction(f, imsize=300):
+    arr = np.zeros([imsize,imsize])
+    for i in range(imsize):
+        for j in range(imsize):
+            xx = ((i - 0.5*imsize) / float(imsize)) * 2
+            yy = ((-j + 0.5*imsize) / float(imsize)) * 2
+            arr[j,i] = f(xx,yy) 
+            
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.tick_params(labelsize=16)
+            
+    plt.imshow(arr, interpolation='nearest', cmap='gray', vmin=0, vmax=1, extent=[-1,1,-1,1])
+    plt.show()
+
+
+def graphfunction(f, precision=300, x1=-1, x2=1, y1=-1, y2=1):
+    arx = np.fromfunction(lambda i: ((i-precision/2)/precision*2), (precision, ))
+    ary = np.vectorize(f)(arx) 
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.tick_params(labelsize=16)
+
+    plt.axis((x1,x2,y1,y2))
+    plt.plot(arx, ary)
+    plt.show()
+    
